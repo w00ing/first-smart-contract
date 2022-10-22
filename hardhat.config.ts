b0b532fcbc3563c1ca5718ab25dec7a29a21d16d
Woo-yeong Kim <kimwy1997@gmail.com>
2022-10-22 11:18:47 +0900

    JS -> TS

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+};
+
+export default config;
