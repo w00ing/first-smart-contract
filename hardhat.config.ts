ff75ddf9460867b8de3e8b95c67cc4ef88f4b2a2
Woo-yeong Kim <kimwy1997@gmail.com>
2022-10-22 13:04:34 +0900

    deploy contract on goerly testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
 task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
   const accounts = await ethers.getSigners();
@@ -11,6 +13,16 @@ task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
